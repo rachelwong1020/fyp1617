859ddf4184302627e12b13361181147475c6dd45
ansonmouse <ansonb06iatd>
2017-03-04 03:21:26 +0800

    Main Game Nav

--- client/layout/MainGameLayout.js
+++ client/layout/MainGameLayout.js
@@ -5,6 +5,9 @@ Template.MainGameLayout.onRendered(function () {
     $('.collapsible').collapsible();
 });
 Template.MainGameLayout.helpers({
+    checkPoint: function () {
+        return Session.get('MainGameCheckPoints');
+    },
     location: function () {
         
     }
