ea192c03cd62c1b1aeaf68a236ce602eb0d28de0
ansonmouse <ansonb06iatd>
2017-03-04 00:15:09 +0800

    Push logo，map

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -1,7 +1,7 @@
 module.exports = {
   servers: {
     one: {
-      host: '188.166.215.119',
+      host: '128.199.102.73',
       username: 'root',
       // pem:
       password: 'ansonb06iatd',
@@ -10,32 +10,30 @@ module.exports = {
   },
 
   meteor: {
-    name: 'racheb_fyp',
+    name: 'rachel_fyp',
     path: '../',
+    docker: {
+      //image: 'kadirahq/meteord', // (optional)
+      image: 'abernix/meteord:base', // use this image if using Meteor 1.4+
+    },
     servers: {
-      one: {},
+      one: {}
     },
     buildOptions: {
       serverOnly: true,
     },
     env: {
-      ROOT_URL: 'app.com',
-      MONGO_URL: 'mongodb://localhost/meteor',
+      ROOT_URL: 'http://rachel_fyp.net',
+      MONGO_URL: 'mongodb://localhost/meteor'
     },
 
-    // change to 'kadirahq/meteord' if your app is not using Meteor 1.4
-    dockerImage: 'abernix/meteord:base',
-    deployCheckWaitTime: 60,
-    
-    // Show progress bar while uploading bundle to server
-    // You might need to disable it on CI servers
-    enableUploadProgressBar: false
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime: 600
   },
 
   mongo: {
     oplog: true,
     port: 27017,
-    version: '3.4.1',
     servers: {
       one: {},
     },
