387ed5e1cc81a7ac25512541fa7cbac1ef297297
ansonmouse <ansonb06iatd>
2017-03-13 22:26:35 +0800

    Sth llm cannot do

--- client/1950/hawker/hawker.js
+++ client/1950/hawker/hawker.js
@@ -1,52 +1,50 @@
 /**
  * Created by Rachel on 8/3/2017.
  */
+var imageRelationship = [
+    {upperNumber: 1, lowerString: 'one', beforeImg: 'central_LiYuenStEast_before.png', afterImg: 'central_LiYuenStEast_after.png'},
+    {upperNumber: 2, lowerString: 'two', beforeImg: 'ssp_PeiHoSt_before.png', afterImg: 'ssp_PeiHoSt_after.png'},
+    {upperNumber: 3, lowerString: 'three', beforeImg: 'ssp_YuChauSt_before.png', afterImg: 'ssp_YuChauSt_after.png'},
+];
+
 var correctCards = 0;
 Template.hawker.onRendered(function () {
     init();
 });
 
 Template.hawker.helpers({
-
+    gameDone: function () {
+        return Session.get('game1950HwakerDone');
+    }
 });
 
 function init() {
-
-    // Hide the success message
-    $('#successMessage').hide();
-    $('#successMessage').css( {
-        left: '580px',
-        top: '250px',
-        width: 0,
-        height: 0
-    } );
-
     // Reset the game
     correctCards = 0;
     $('#cardPile').html( '' );
     $('#cardSlots').html( '' );
 
     // Create the pile of shuffled cards
-    var numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+    var numbers = [1, 2, 3];
     numbers.sort( function() { return Math.random() - .5 } );
 
-    for ( var i=0; i<10; i++ ) {
-        $('<div>' + numbers[i] + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card'+numbers[i] ).appendTo( '#cardPile' ).draggable( {
-            containment: '#content',
+    for (var i = 0; i < 3; i++) {
+        $('<div></div>').data( 'number', numbers[i] ).attr( 'id', 'card'+numbers[i] ).appendTo( '#cardPile' ).draggable( {
+            containment: '#hawker_content',
             stack: '#cardPile div',
             cursor: 'move',
             revert: true
-        } );
+        } ).addClass('hoverable').css('background-image', 'url(/resources/games/Game1950hawker/img/'+imageRelationship[numbers[i]-1].beforeImg+')');
     }
 
     // Create the card slots
-    var words = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten' ];
-    for ( var i=1; i<=10; i++ ) {
-        $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
+    var words = [ 'one', 'two', 'three'];
+    for ( var i=1; i<=3; i++ ) {
+        $('<div></div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
             accept: '#cardPile div',
             hoverClass: 'hovered',
             drop: handleCardDrop
-        } );
+        } ).css('background-image', 'url(/resources/games/Game1950hawker/img/'+imageRelationship[i-1].afterImg+')');
     }
 
 }
@@ -72,15 +70,8 @@ function handleCardDrop( event, ui ) {
     // If all the cards have been placed correctly then display a message
     // and reset the cards for another go
 
-    if ( correctCards == 10 ) {
-        $('#successMessage').show();
-        $('#successMessage').animate( {
-            left: '380px',
-            top: '200px',
-            width: '400px',
-            height: '100px',
-            opacity: 1
-        } );
+    if ( correctCards == 3 ) {
+        Session.set('game1950HwakerDone', true);
     }
 
 }
