5e94f8b4f49f2362c8eabfe81248a470d8396910
ansonmouse <ansonb06iatd>
2017-04-25 07:12:18 +0800

    da bao wayne

--- client/1960/lamtsuen/Game1960Lamtsuen.js
+++ client/1960/lamtsuen/Game1960Lamtsuen.js
@@ -3,6 +3,10 @@
  */
 Template.Game1960Lamtsuen.onCreated(function () {
     Session.set('Game1960LamtsuenDameStage', 0);
+    Session.set('Game1960LamtsuenCompletedCount', 0);
+    Session.set('Game1960LamtsuenSuccessCount', 0);
+    Session.set('Game1960LamtsuenStarted', false);
+    Session.set('game1960LamtsuenStartedPageOne', false);
 });
 var energyPointer;
 var energyPointerMinPosition = 25;
@@ -13,46 +17,63 @@ var prayitem;
 var prayitemInitVelocity;
 var prayitemInitBottom = 100;
 var prayitemCurrentPosition = prayitemInitBottom;
-var prayitemMinHozPosition = 15;
+var prayitemMinHozPosition = 20;
 var prayitemMaxHozPosition = 70;
 var prayitemCurrentHozPosition = prayitemMinHozPosition;
 var prayitemMovingFactor = 1;
 var throwFactor;
+var key;
 Template.Game1960Lamtsuen.onRendered(function () {
     startPrayitemHozMove();
-    $(document).keypress(function (e) {
-        if(e.keyCode == 32) {
-            switch (Session.get('Game1960LamtsuenDameStage')) {
-                case 0:
-                    clearInterval(prayitem);
-                    startEnergyBar();
-                    Session.set('Game1960LamtsuenDameStage', Session.get('Game1960LamtsuenDameStage')+1);
-                    break;
-                case 1:
-                    MainGame.increaseMp(-100);
-                    clearInterval(energyPointer);
-                    var energyLevel;
-                    if(energyPointerCurrentPosition <= 39.75) {
-                        energyLevel = (energyPointerCurrentPosition - energyPointerMinPosition)/(39.75-energyPointerMinPosition);
-                        throwFactor = energyLevel*9;
-                    } else {
-                        if(energyPointerCurrentPosition <= 56.3) {
-                            energyLevel = (energyPointerCurrentPosition - 39.75)/(56.3-39.75);
-                            throwFactor = energyLevel*6+9;
+    key = $(document).keypress(function (e) {
+        if(e.keyCode == 32 && Session.get('Game1960LamtsuenStarted')) {
+            if(Session.get('MainGameMP') >= 100) {
+                switch (Session.get('Game1960LamtsuenDameStage')) {
+                    case 0:
+                        clearInterval(prayitem);
+                        startEnergyBar();
+                        Session.set('Game1960LamtsuenDameStage', Session.get('Game1960LamtsuenDameStage')+1);
+                        break;
+                    case 1:
+                        MainGame.increaseMp(-100);
+                        clearInterval(energyPointer);
+                        var energyLevel;
+                        console.log(energyPointerCurrentPosition);
+                        if(energyPointerCurrentPosition <= 40) {
+                            energyLevel = (energyPointerCurrentPosition - energyPointerMinPosition)/(40-energyPointerMinPosition);
+                            throwFactor = energyLevel*6;
                         } else {
-                            energyLevel = (energyPointerCurrentPosition - 56.3)/(energyPointerMaxPosition-56.3);
-                            throwFactor = energyLevel*5+15;
+                            if(energyPointerCurrentPosition <= 57) {
+                                energyLevel = (energyPointerCurrentPosition - 40)/(57-40);
+                                throwFactor = energyLevel*7+6;
+                            } else {
+                                energyLevel = (energyPointerCurrentPosition - 57)/(energyPointerMaxPosition-57);
+                                throwFactor = energyLevel*5+13;
+                            }
                         }
-                    }
-                    prayitemInitVelocity = throwFactor;
-                    startPrayitemThrow();
-                    break;
+                        prayitemInitVelocity = throwFactor;
+                        startPrayitemThrow();
+                        break;
+                }
             }
         }
     });
 });
 
+Template.Game1960Lamtsuen.onDestroyed(function () {
+    key = null;
+});
+
 Template.Game1960Lamtsuen.helpers({
+    gameDone: function () {
+        return Session.get('Game1960LamtsuenCompletedCount') == 5;
+    },
+    gameStarted: function () {
+        return Session.get('Game1960LamtsuenStarted');
+    },
+    pageOne: function () {
+        return !Session.get('game1960LamtsuenStartedPageOne');
+    },
     showEnergyBar: function () {
         return Session.get('Game1960LamtsuenDameStage') != 0;
     },
@@ -62,6 +83,14 @@ Template.Game1960Lamtsuen.helpers({
 });
 
 Template.Game1960Lamtsuen.events({
+    'click #1960_lamtsuen_leave': function () {
+        var completed = Session.get('MainGameCompletedCheckPoint');
+        if(completed.indexOf(Session.get('MainGameCurrentGame')) < 0) {
+            completed.push(Session.get('MainGameCurrentGame'));
+            Session.set('MainGameCompletedCheckPoint', completed);
+        }
+        Session.set('MainGameCurrentGame', null);
+    },
     'click #restart_game': function () {
         energyPointerCurrentPosition = energyPointerMinPosition;
         energyPointerMovingFactor = 1;
@@ -72,6 +101,12 @@ Template.Game1960Lamtsuen.events({
         $('#prayitem').css('left', prayitemCurrentHozPosition+'%').css('bottom', prayitemInitBottom);
         startPrayitemHozMove();
         Session.set('Game1960LamtsuenDameStage', 0);
+    },
+    'click #start_1960_lamtsuen_game': function () {
+        Session.set('Game1960LamtsuenStarted', true);
+    },
+    'click #start_1960_lamtsuen_next': function () {
+        Session.set('game1960LamtsuenStartedPageOne', true);
     }
 });
 
@@ -109,15 +144,20 @@ function startPrayitemThrow() {
             prayitemCurrentPosition = prayitemInitBottom;
             clearInterval(prayitem);
             Session.set('Game1960LamtsuenDameStage', Session.get('Game1960LamtsuenDameStage')+1);
+            Session.set('Game1960LamtsuenCompletedCount', Session.get('Game1960LamtsuenCompletedCount')+1);
         }
         $('#prayitem').css('bottom', prayitemCurrentPosition);
-        if(throwFactor >= 9 && throwFactor <= 15 && prayitemInitVelocity <= 0) {
+        if(throwFactor >= 6 && throwFactor <= 13 && prayitemInitVelocity <= 0) {
             clearInterval(prayitem);
             Session.set('Game1960LamtsuenDameStage', Session.get('Game1960LamtsuenDameStage')+1);
+            MainGame.increaseHp(100);
+            Session.set('Game1960LamtsuenSuccessCount', Session.get('Game1960LamtsuenSuccessCount')+1);
+            Session.set('Game1960LamtsuenCompletedCount', Session.get('Game1960LamtsuenCompletedCount')+1);
         }
-        if(throwFactor >= 16 && prayitemInitVelocity <= 0) {
+        if(throwFactor >= 14 && prayitemInitVelocity <= 0) {
             clearInterval(prayitem);
             Session.set('Game1960LamtsuenDameStage', Session.get('Game1960LamtsuenDameStage')+1);
+            Session.set('Game1960LamtsuenCompletedCount', Session.get('Game1960LamtsuenCompletedCount')+1);
         }
     }, 10);
 }
